4965a85957b4c18d341d0db1adbc99cad46f8045
javierspr <learn.javier@gmail.com>
2024-08-20 16:22:01 -0300

    add link products & newProduct

--- client/src/pages/NewProduct.tsx
+++ client/src/pages/NewProduct.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
 
 export default function NewProduct() {
   return (
-    <div>NewProduct</div>
+    <>
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-black text-slate-600 uppercase">Register new product</h2>
+        <Link
+          to="/"
+          className="rounded-md p-3 bg-blue-700 hover:bg-blue-500 text-sm text-white shadow-sm"
+        >
+          Go Back To Products
+        </Link>
+      </div>
+    </>
   )
 }
--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-black text-slate-600 uppercase">Products</h2>
+        <Link
+          to="products/new"
+          className="rounded-md p-3 bg-blue-700 hover:bg-blue-500 text-sm text-white shadow-sm"
+        >
+          Add Product
+        </Link>
+      </div>
+    </>
   )
 }
