a6130453d5c21d5b6b2e86f15904da51c49d54fd
javierspr <learn.javier@gmail.com>
2024-08-20 16:14:59 -0300

    add layout

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -3,9 +3,17 @@ import { Outlet } from "react-router-dom"
 export default function Layout() {
   return (
     <>
-        <div>From Layout</div>
+        <header className="bg-slate-800">
+            <div className="mx-auto max-w-5xl py-10 px-2 lg:px-0">
+                <h1 className="text-4xl font-extrabold text-white">
+                    Products Administrator
+                </h1>
+            </div>
+        </header>
 
-        <Outlet/>
+        <main className="my-10 mx-auto max-w-5xl p-10 bg-white shadow-md">
+          <Outlet/>
+        </main>
     </>
   )
 }
