667dc45a2f0e9b18a1ad95dd2750316314c61147
javierspr <learn.javier@gmail.com>
2024-08-22 19:28:31 -0300

    add addProduct to ProductService

--- client/src/pages/NewProduct.tsx
+++ client/src/pages/NewProduct.tsx
@@ -1,5 +1,6 @@
 import { Link, Form, useActionData, ActionFunctionArgs } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
+import { addProduct } from "../services/ProductService";
 
 export async function action({ request } : ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData()) 
@@ -12,6 +13,8 @@ export async function action({ request } : ActionFunctionArgs) {
   if(error.length) {
     return error
   }
+
+  addProduct(data)
 
   return {}
 }
