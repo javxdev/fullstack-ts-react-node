f57bfa1c211161fcec2c61f7cc05ceceec5cb1de
javierspr <learn.javier@gmail.com>
2024-08-23 17:10:33 -0300

    add DraftProductSchema

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,7 +1,22 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../types"
+
 type ProductData = {
     [k: string]: FormDataEntryValue;
 }
 
 export const addProduct = async (data : ProductData) => {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        })
+        if(result.success) {
+            console.log('valid data')
+        } else {
+            throw new Error('not valid data')
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
