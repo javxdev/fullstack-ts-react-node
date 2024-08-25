c0eeb30de021ba26db64707f0510b3558f909eb1
javierspr <learn.javier@gmail.com>
2024-08-25 20:30:07 -0300

    add redirect to index when adding a product

--- client/src/pages/NewProduct.tsx
+++ client/src/pages/NewProduct.tsx
@@ -1,4 +1,4 @@
-import { Link, Form, useActionData, ActionFunctionArgs } from "react-router-dom";
+import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductService";
 
@@ -14,9 +14,9 @@ export async function action({ request } : ActionFunctionArgs) {
     return error
   }
 
-  addProduct(data)
-
-  return {}
+  await addProduct(data)
+  
+  return redirect('/')
 }
 
 export default function NewProduct() {
