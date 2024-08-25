1a1e2aeac7eef7b6a843f884ddb088894ad858e9
javierspr <learn.javier@gmail.com>
2024-08-24 21:45:36 -0300

    add cors options

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import router from './router'
 import db from './config/db'
 import colors from 'colors'
-import cors from 'cors'
+import cors, { CorsOptions } from 'cors'
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec, { swaggerUiOptions } from './config/swagger'
 
@@ -22,6 +22,18 @@ connectDB()
 // Express Instance
 const server = express()
 
+// CORS CONFIG
+const corsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        }else {
+            callback(new Error('CORS error'))
+        }
+    } 
+}
+
+server.use(cors(corsOptions))
 
 // Reading data from forms
 server.use(express.json())
