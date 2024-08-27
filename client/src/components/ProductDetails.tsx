4befe5a51890eed453687034c6c94b375dcb1047
javierspr <learn.javier@gmail.com>
2024-08-27 17:52:17 -0300

    add useLoaderData

---
+++ client/src/components/ProductDetails.tsx
@@ -0,0 +1,19 @@
+
+export default function ProductDetails() {
+  return (
+    <tr className="border-b ">
+        <td className="p-3 text-lg text-gray-800">
+
+        </td>
+        <td className="p-3 text-lg text-gray-800">
+
+        </td>
+        <td className="p-3 text-lg text-gray-800">
+
+        </td>
+        <td className="p-3 text-lg text-gray-800 ">
+        
+        </td>
+    </tr> 
+  )
+}
--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, useLoaderData } from "react-router-dom"
 import { getProducts } from "../services/ProductService"
+import ProductDetails from "../components/ProductDetails"
 
 export async function loader(){
   const products = await getProducts()
-  console.log(products)
-  return { }
+  
+  return products
 }
 
 export default function Products() {
+
+  const products = useLoaderData()
+  console.log(products)
+
   return (
     <>
       <div className="flex justify-between items-center">
@@ -18,6 +23,26 @@ export default function Products() {
         >
           Add Product
         </Link>
+      </div>
+
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+              <tr>
+                  <th className="p-2">Producto</th>
+                  <th className="p-2">Precio</th>
+                  <th className="p-2">Disponibilidad</th>
+                  <th className="p-2">Acciones</th>
+              </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <ProductDetails
+                product={product}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   )
