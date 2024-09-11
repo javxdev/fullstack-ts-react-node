46420c39bbaf5c717f287496449688afd7e4ee60
javierspr <javxdev@gmail.com>
2024-09-11 08:51:00 -0300

    add configs for deployment

--- client/src/components/ErrorMessage.tsx
+++ client/src/components/ErrorMessage.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function ErrorMessage({children} : PropsWithChildren) {
   return (
-    <div className=" text-center my-5 bg-red-600 text-sm text-white uppercase font-bold p-3">
+    <div className=" text-center mt-10 bg-red-600 text-sm text-white uppercase font-bold p-3">
         {children}
     </div>
   )
