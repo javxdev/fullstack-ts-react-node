b3a670c6baf30150b0e58260e57aec195b05729d
javierspr <javxdev@gmail.com>
2024-09-18 16:57:06 -0300

    update mobile interface

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "./layouts/Layout";
-import Products, { loader as productsLoader, action as updateAvailabilityAction} from "./pages/Products";
-import NewProduct, { action as newProductAction } from "./pages/NewProduct";
-import EditProduct, { loader as editProductLoader, action as editProductAction } from "./pages/EditProduct";
-import { action as deleteProductAction } from "./components/ProductDetails";
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import Products, { loader as productsLoader, action as updateAvailabilityAction } from './pages/Products'
+import NewProduct, { action as newProductAction } from './pages/NewProduct'
+import EditProduct, { loader as editProductLoader, action as editProductAction } from './pages/EditProduct'
+import { action as deleteProductAction } from './components/ProductDetails'
 
 export const router = createBrowserRouter([
     {
